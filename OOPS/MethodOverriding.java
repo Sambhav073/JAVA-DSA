026e327fa133170bcffa73e33c43929f02a2471e
Sambhav073 <sambhavchoudhary073@gmail.com>
2025-07-10 03:46:33 +0530

    resolved a file

--- OOPS/MethodOverriding.java
+++ OOPS/MethodOverriding.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> ff6d9552665b2f2d3bfc3d74ac823c62132f5e0f
 class Calc{
     int add(int a, int b){
         
@@ -26,31 +23,3 @@ public class MethodOverriding {
         System.out.println(c2.add(2, 3)); // Output: 6
     }
 }
-<<<<<<< HEAD
-
-// class Calc{
-//     int add(int a, int b){
-        
-//         return a + b;
-//     }
-// }
-
-// class AdvCalc extends Calc{
-//     int add(int a, int b){
-//         return a + b + 1;
-//     }
-    
-// }
-
-// public class MethodOverriding {
-//     public static void main(String[] args) {
-//         Calc c = new Calc();
-//         System.out.println(c.add(2, 3)); // Output: 5
-//         AdvCalc ac = new AdvCalc();
-//         System.out.println(ac.add(2, 3)); // Output: 6
-//         Calc c2 = new AdvCalc();
-//         System.out.println(c2.add(2, 3)); // Output: 6
-//     }
-// }
-=======
->>>>>>> ff6d9552665b2f2d3bfc3d74ac823c62132f5e0f
