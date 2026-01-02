99efd891058afbe1b65020d9cd00526fb0810737
Sambhav073 <sambhavchoudhary073@gmail.com>
2026-01-02 11:33:01 +0530

    jdbc added

--- JDBC/FirstConnection/src/main/java/org/example/Main.java
+++ JDBC/FirstConnection/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
-
+import java.sql.*;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+
+        String sql="SELECT customer_id FROM payment where amount=0";
+        String url="jdbc:postgresql://localhost:5432/dvdrental";
+        String username="postgres";
+        String password="Sambhav@073";
+
+        Connection con=DriverManager.getConnection(url,username,password);
+        Statement st= con.createStatement();
+        ResultSet rs=st.executeQuery(sql);
+
+        while(rs.next()){
+            int id=rs.getInt(1);
+            System.out.println(id);
+        }
+        con.close();
     }
 }
---
+++ Trie/Main.java
@@ -0,0 +1,49 @@
+import java.net.StandardSocketOptions;
+
+public class Main {
+
+    static class Node{
+        Node[] children =new Node[26];
+        boolean isEndOfWord=false;
+        Node(){
+            for(int i=0;i<26;i++){
+                this.children[i]=null;
+            }
+        }
+    }
+
+    public static void insert(String word){
+        Node curr=root;
+        for(int level=0;level<word.length();level++){
+            int idx=word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
+            }
+            curr=curr.children[idx];
+        }
+        curr.isEndOfWord=true;
+    }
+
+    public static boolean search(String word){
+        Node curr=root;
+        for(int level=0;level<word.length();level++){
+            int idx=word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr=curr.children[idx];
+        }
+        return curr.isEndOfWord;
+    }
+
+    static Node root=new Node();
+
+    public static void main(String[] args) {
+        String[] words ={"the","a","there","their","any","thee"};
+        for (String word : words) {
+            insert(word);
+        }
+        System.out.println(search("thee"));
+        System.out.println(search("thor"));
+    }
+}
